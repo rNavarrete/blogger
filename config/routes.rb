f02bece132eaa2b3ba8c9f4fa8c08e03b372a918
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-21 21:49:45 -0600

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   end
 
   resources :tags
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
   root to: 'articles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
